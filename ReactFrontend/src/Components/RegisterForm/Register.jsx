6c626aef1bf7173ffbfd5aad4042bd654844a482
ChelseaOkoroji <159299248+ChelseaOkoroji@users.noreply.github.com>
2024-10-09 15:37:12 -0400

    pushing files

---
+++ ReactFrontend/src/Components/RegisterForm/Register.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Register.css'
+
+const Register = () => {
+  const [userID, setUserID] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    const user = { userID, email, password };
+    fetch('http://localhost:8000/users/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('User registered:', data);
+      })
+      .catch(error => console.error('Error:', error));
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={userID}
+        onChange={(e) => setUserID(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+};
+
+export default Register;
