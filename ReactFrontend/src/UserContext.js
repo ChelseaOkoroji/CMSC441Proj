5b262aeba80c4fb691ab2b51a5f32a4a6411b6da
Trevor Stepp <181792165+trevorstepp@users.noreply.github.com>
2024-10-22 18:55:00 -0400

    Small UserContext change

--- ReactFrontend/src/UserContext.js
+++ ReactFrontend/src/UserContext.js
@@ -2,7 +2,15 @@ import React, { createContext, useContext, useState } from 'react';
 
 const UserContext = createContext();
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+        // Check local storage
+        const savedUser = localStorage.getItem('user');
+        if(savedUser) {
+            return JSON.parse(savedUser)
+        }
+        return null;
+    });
+
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
