55c456dba077fa79aa59b3df4b62478ae89b9e0a
ChelseaOkoroji <159299248+ChelseaOkoroji@users.noreply.github.com>
2024-10-28 14:00:12 -0400

    Adding category bar

--- ReactFrontend/src/Components/HomePage/HomePage.jsx
+++ ReactFrontend/src/Components/HomePage/HomePage.jsx
@@ -14,9 +14,10 @@ const HomePage = () => {
         };
       }, []);
     const { user } = useUser(); // Keep this
+    const [menu, setMenu] = useState();
     /*Everything in here can be deleted, was just for testing purposes to see if login was working*/
     return (
-        <div>
+        <div className='homepage'>
             <header style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
                 <h1>Product List</h1>
                 <div>
@@ -24,6 +25,15 @@ const HomePage = () => {
                     <Link to="/profile" style={{ marginLeft: '10px' }}>
                         <button>Profile</button>
                     </Link>
+                    <ul className='menu'>
+                        <li onClick={()=>{setMenu("all")}}>All{menu==="all"?<hr/>:<></>}</li>
+                        <li onClick={()=>{setMenu("book")}}>Book{menu==="book"?<hr/>:<></>}</li>
+                        <li onClick={()=>{setMenu("merch")}}>Merch{menu==="merch"?<hr/>:<></>}</li>
+                        <li onClick={()=>{setMenu("school-supplies")}}>School Supplies{menu==="school-supplies"?<hr/>:<></>}</li>
+                        <li onClick={()=>{setMenu("technology")}}>Technology{menu==="technology"?<hr/>:<></>}</li>
+                        <li onClick={()=>{setMenu("dorm")}}>Dorm{menu==="dorm"?<hr/>:<></>}</li>
+                        <li onClick={()=>{setMenu("health")}}>Health/Fitness{menu==="health"?<hr/>:<></>}</li>
+                    </ul>     
                 </div>
             </header>
             {/* Here you would map over your products and display them */}
